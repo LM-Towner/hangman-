79ee2aeeca7cea207dea557261777e487fc35988
La-Keisha Towner <ltowner622@gmail.com>
2017-05-23 20:08:48 -0400

    added logic for testing to see if letter is in the word

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -1,21 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hangman;
 
-/**
- *
- * @author quietstorm
- */
 public class Hangman {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        Game game = new Game("Fun");
+        Prompter prompter = new Prompter(game);
+        boolean isHit = prompter.promptForGuess();
+        if (isHit){
+            System.out.println("You guessed a letter");
+        }
+        else{
+            System.out.println("No letter this time");
+        }
     }
     
 }
